9f667469f4b696d3870cd747221fc0def58f0838
DonGeo <geo.du07@gmail.com>
2021-09-14 14:44:50 -0500

    firt commit to inicializate

---
+++ src/styles.js
@@ -0,0 +1,19 @@
+export const colors = {
+  primary: "#5539a3",
+  secondary: "#FFD543",
+  accent: "#FF6123",
+  gray1: "#C0C0C0",
+  gray2: "#575757",
+  black: "#000",
+  white: "#fff",
+}
+
+export const breakpoints = {
+  "desktop-medium": 1440,
+  "desktop-small": 1200,
+  "tablet-large": 900,
+  "tablet-medium": 768,
+  "mobile-medium": 700,
+  "mobile-small": 360,
+  "mobile-xsmall": 320,
+}
